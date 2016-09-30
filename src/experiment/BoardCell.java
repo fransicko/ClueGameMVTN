611d05729642487a17a6516b70b741f2723a6abf
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-09-30 11:12:33 -0600

    So i'm just fleshing out the functions. It says that we need to sumbit the failed tests so at the moment i think we just make sure that the constructors are working then make the test and sumbit the errors

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -3,5 +3,12 @@ package experiment;
 public class BoardCell {
 	private int row;
 	private int column;
+	
+	public BoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
 
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,11 +1,35 @@
 package experiment;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class IntBoard {
-	private Map<BoardCell, Set<BoardCell>> adjMtx;
+	// the key is the current cell and the values are the Adjacent cells
+	private Map<BoardCell, Set<BoardCell>> adjMtx; 
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
 	private BoardCell[][] grid;
+	
+	public IntBoard(int ROWS, int COLS) {
+		super();
+		grid = new BoardCell[ROWS][COLS];
+		for (int i = 0; i < ROWS; ++i) {
+			for (int j = 0; j < COLS; ++j) {
+				grid[i][j] = new BoardCell(i, j);
+			}
+		}
+		visited = new HashSet<BoardCell>();
+		targets = new HashSet<BoardCell>();
+		adjMtx = new HashMap<BoardCell, Set<BoardCell>>();
+	}
+
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void calcTargets(BoardCell startCell, int pathLength) {
+		
+	}
 }
