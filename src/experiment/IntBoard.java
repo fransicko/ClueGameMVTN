6247dce0910801efa26a27dd34bfab4ef10babba
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-09-30 10:38:37 -0600

    I'm just adding the stuff into the classes. Need to see what we should add to a constructor.

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,11 @@
 package experiment;
 
-public class IntBoard {
+import java.util.Map;
+import java.util.Set;
 
+public class IntBoard {
+	private Map<BoardCell, Set<BoardCell>> adjMtx;
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
+	private BoardCell[][] grid;
 }
