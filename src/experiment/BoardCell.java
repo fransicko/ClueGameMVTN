3dcd2e4ec8f6f8f59d27ee13f4166313fac48e89
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-10-03 11:36:48 -0600

    I added some getters for BoardCell and i added some private variables to IntBoard so that way we know how big the game board is. Started working on clacAdjacencies did south check.

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -9,6 +9,13 @@ public class BoardCell {
 		this.row = row;
 		this.column = column;
 	}
-	
 
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+	
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -11,10 +11,15 @@ public class IntBoard {
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
 	private BoardCell[][] grid;
+	// These values are to be used for comparing the location of the board cell.
+	private int rowBounds;	
+	private int colBounds;
 	
 	public IntBoard(int ROWS, int COLS) {
 		super();
 		grid = new BoardCell[ROWS][COLS];
+		rowBounds = ROWS;
+		colBounds = COLS;
 		for (int i = 0; i < ROWS; ++i) {
 			for (int j = 0; j < COLS; ++j) {
 				grid[i][j] = new BoardCell(i, j);
@@ -26,15 +31,24 @@ public class IntBoard {
 	}
 
 	public void calcAdjacencies() {
+		Set<BoardCell> adj = new HashSet<BoardCell>();
+		// We first find good values then we will add them later
+		for (BoardCell i: visited) {
+			if (i.getRow()+1 < rowBounds) {
+				adj.add(grid[i.getRow()+1][i.getColumn()]);
+			}
+		}
 		return;
 	}
 	
 	public void calcTargets(BoardCell startCell, int pathLength) {
+		// First we calculate the adjancient cells
+		calcAdjacencies();
 		return;
 	}
 	
 	public Set<BoardCell> getTargets() {
-		return null;
+		return targets;
 	}
 	
 	public Set<BoardCell> getAdjList(BoardCell cell) {
