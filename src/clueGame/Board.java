8bee17697928d8446b6bdcd95a71f5ff6ffa58b8
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-10-04 11:42:12 -0600

    added things to clueGame

---
+++ src/clueGame/Board.java
@@ -0,0 +1,16 @@
+package clueGame;
+
+import java.util.Map;
+import java.util.Set;
+
+import experiment.BoardCell;
+
+public class Board {
+	public static final int MAX_BOARD_SIZE = 50;
+	private int numRows;
+	private int numColumns;
+	private BoardCell[][] board;
+	private Map<Character, String> rooms;
+	private Map<BoardCell, Set<BoardCell>> adjMatrix;
+
+}
