ecadb1acb00445df5d26092de3fc483e5a33994d
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-10-10 17:50:34 -0600

    I finished test adj inside room, and i updated the spreadsheet

--- src/tests/clueBoardAdjTargetTests.java
+++ src/tests/clueBoardAdjTargetTests.java
@@ -3,11 +3,13 @@ package tests;
 import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
+import java.util.Set;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import clueGame.Board;
+import clueGame.BoardCell;
 
 public class clueBoardAdjTargetTests {
 	private static Board board;
@@ -23,8 +25,25 @@ public class clueBoardAdjTargetTests {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdjacenciesInsideRooms() {
+		//Test side
+		Set<BoardCell> test = board.getAdjList(board.getCell(9, 0));
+		assertEquals(0, test.size());
+		//Test one that has a hallway underneath
+		test = board.getAdjList(board.getCell(13, 0));
+		assertEquals(0, test.size());
+		// Test one that has walkway above
+		test = board.getAdjList(board.getCell(8, 9));
+		assertEquals(0, test.size());
+		// Test one that is in middle of room
+		test = board.getAdjList(board.getCell(21, 11));
+		assertEquals(0, test.size());
+		// Test one beside a door
+		test = board.getAdjList(board.getCell(2, 15));
+		assertEquals(0, test.size());
+		// Test one in a corner of room
+		test = board.getAdjList(board.getCell(25, 20));
+		assertEquals(0, test.size());
 	}
 
 }
